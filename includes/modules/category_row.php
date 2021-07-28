f231765baeadaea789babc3797713a779dc85184
Scott C Wilson <scottcwilson@gmail.com>
2021-07-28 08:52:05 -0400

    Add notifier when category image empty

--- includes/modules/category_row.php
+++ includes/modules/category_row.php
@@ -28,7 +28,12 @@ if ($num_categories > 0) {
   }
 
   while (!$categories->EOF) {
-    if (!$categories->fields['categories_image']) $categories->fields['categories_image'] = 'pixel_trans.gif';
+     if (empty($categories->fields['categories_image'])) {
+        $zco_notifier->notify('NOTIFY_CATEGORY_ROW_EMPTY_IMAGE', $categories->fields['categories_id'], $categories->fields['categories_image']); 
+        if (empty($categories->fields['categories_image'])) {
+           $categories->fields['categories_image'] = 'pixel_trans.gif';
+        }
+     }
     $cPath_new = zen_get_path($categories->fields['categories_id']);
 
     // strip out 0_ from top level cats
